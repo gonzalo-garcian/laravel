30d9974daae3da732375deb4e81f11e46a0966cd
gonzalo-garcian <gonzalo.garcian@autonoma.cat>
2023-01-01 19:42:02 +0100

    new file:   views/partials/head.view.php

--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-
-<html lang="en" class="h-full bg-gray-100">
-
-<head>
-
-	<meta charset="UTF-8">
-
-	<title>Document</title>
-
-	<script src="https://cdn.tailwindcss.com"></script>
-
-</head>
+<?php require('partials/head.view.php'); ?>
 
 <body class="h-full">
 
