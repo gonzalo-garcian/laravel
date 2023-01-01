c612cb14ff0d5b53fa5f2bb277695beb17cf6adb
gonzalo-garcian <gonzalo.garcian@autonoma.cat>
2023-01-01 19:27:12 +0100

    modified:   ourmission.view.php

--- contact.view.php
+++ contact.view.php
@@ -27,12 +27,12 @@
 
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Our Mission</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
     </div>
   </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p>Now you are on the Our Mission page.</p>
+      <p>Now you are on the Contact page.</p>
     </div>
   </main>
 </div>
