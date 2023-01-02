d8b0abd95e50bfd2eb2da45aa708662f623e6ec1
gonzalo-garcian <gonzalo.garcian@autonoma.cat>
2023-01-02 21:14:33 +0100

    new file: router.php

---
+++ router.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$routes = [
+	'/laravel/' => 'controllers/index.php',
+	'/laravel/about' => 'controllers/about.php',
+	'/laravel/contact' => 'controllers/contact.php',
+	'/laravel/ourmission' => 'controllers/ourmission.php',
+];
+
+$route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if(array_key_exists($route, $routes)){
+
+	require $routes[$route];
+
+}
+else {
+
+	require('controllers/404.php');
+}
+
