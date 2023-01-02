df74682270b5c9fff4c8ee0de5df857a011c9574
gonzalo-garcian <gonzalo.garcian@autonoma.cat>
2023-01-02 21:19:49 +0100

    modified: index.php

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ $statement->execute();
 
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-dd($posts);
+foreach($posts as $post){
+
+    echo "<h1> {$post['name']}, {$post['grade']} {$post['scale']} ({$post['collection']}) </h1>";
+
+}
 
 ?>
