29c5fba2bbece4f57673b98f2f93abc08f54a158
gonzalo-garcian <gonzalo.garcian@autonoma.cat>
2023-01-04 00:09:28 +0100

    modified: notes

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,5 +1,10 @@
 <?php
 
+$db = new Database(require('config.php'));
+$posts = $db -> query('SELECT * FROM note WHERE id = 1') -> fetchAll();
+
+dd($posts);
+
 $header = 'Notes';
 
 require "views/notes.view.php";
--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
 require('functions.php');
-require('router.php');
 require('Database.php');
-
-
-$db = new Database(require('config.php'));
-$posts = $db -> query('SELECT * FROM gunpla WHERE id = :id', [':id' => $_GET['id']]) -> fetchAll();
-
-
-foreach($posts as $post){
-
-    echo "<h1> {$post['name']}, {$post['grade']} {$post['scale']} ({$post['collection']}) </h1>";
-
-}
-
+require('router.php');
