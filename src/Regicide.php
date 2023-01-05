1644945a99f94774a9d2925e49f236eff5035f6c
gonzalo-garcian <gonzalo.garcian@autonoma.cat>
2023-01-05 21:53:11 +0100

    new file: Regicide

---
+++ src/Regicide.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once ('src/Deck.php');
+
+class Regicide extends Deck
+{
+    private static array $pips = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    private static array $suits = ['♠', '♥', '♣', '♦'];
+
+    private static array $castlePips = ['J', 'Q', 'K'];
+
+    private static array $castle;
+
+    public function __construct() {
+        foreach (Regicide::$pips as $pip){
+            foreach (Regicide::$suits as $suit){
+                $this -> cards[] = new Card($suit, $pip);
+            }
+        }
+    }
+}
